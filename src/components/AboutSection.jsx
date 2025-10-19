1cce823bb0a639f62ec6b423829bd3555f76b7fc
pshegde123 <pradnyahegde@gmail.com>
2025-10-18 22:53:02 -0400

    Updated icons

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,4 +1,4 @@
-import { Briefcase, Code, User } from "lucide-react";
+import { Code, FileCode2, Database, FileJson } from "lucide-react";
 
 export const AboutSection = () => {
     return (
@@ -12,7 +12,7 @@ export const AboutSection = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
                     <div className="space-y-6">
                         <h3 className="text-2xl font-semibold">
-                            Passionate Web Developer & Tech Creator
+                            Passionate Web Developer
                         </h3>
 
                         <p className="text-muted-foreground">
@@ -50,7 +50,7 @@ export const AboutSection = () => {
                                     <Code className="h-6 w-6 text-primary" />
                                 </div>
                                 <div className="text-left">
-                                    <h4 className="font-semibold text-lg"> Web Development</h4>
+                                    <h4 className="font-semibold text-lg"> Front-End Development</h4>
                                     <p className="text-muted-foreground">
                                         Creating responsive websites and web applications with
                                         modern frameworks.
@@ -61,10 +61,10 @@ export const AboutSection = () => {
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <User className="h-6 w-6 text-primary" />
+                                    <FileJson className="h-6 w-6 text-primary" />
                                 </div>
                                 <div className="text-left">
-                                    <h4 className="font-semibold text-lg">UI/UX Design</h4>
+                                    <h4 className="font-semibold text-lg">Back-End Development</h4>
                                     <p className="text-muted-foreground">
                                         Designing intuitive user interfaces and seamless user
                                         experiences.
@@ -75,11 +75,12 @@ export const AboutSection = () => {
                         <div className="gradient-border p-6 card-hover">
                             <div className="flex items-start gap-4">
                                 <div className="p-3 rounded-full bg-primary/10">
-                                    <Briefcase className="h-6 w-6 text-primary" />
+                                    <Database className="h-6 w-6 text-primary" />
+                                    <FileCode2 className="h-6 w-6 text-primary" />
                                 </div>
 
                                 <div className="text-left">
-                                    <h4 className="font-semibold text-lg">Project Management</h4>
+                                    <h4 className="font-semibold text-lg">C/C++,Database Development</h4>
                                     <p className="text-muted-foreground">
                                         Leading projects from conception to completion with agile
                                         methodologies.
